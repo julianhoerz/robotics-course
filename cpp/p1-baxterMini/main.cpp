11111288c2602756a6c3129b4830d14717f3f442
Julian <julianhoerz@gmail.com>
2019-04-23 18:27:12 +0200

    test

--- cpp/p1-baxterMini/main.cpp
+++ cpp/p1-baxterMini/main.cpp
@@ -36,8 +36,8 @@ void spline_use(){
 
   RobotOperation B(K);
   cout <<"joint names: " <<B.getJointNames() <<endl;
-  B.move({q_zero,q_home}, {5.,10.});
-  B.move({q_zero}, {15.}); //appends
+  B.move({q_zero,q_home}, {5.,10.},true);
+  B.move({q_zero}, {15.},true); //appends
   B.wait();
   rai::wait();
 
@@ -49,12 +49,303 @@ void spline_use(){
 }
 
 
+
+arr motionProfile(arr y_start,arr y_final,int i,double steps){
+  arr diff = y_final - y_start;
+  diff *= (i/steps);
+  return y_start + diff;
+}
+
+
+
+void graspObject(){
+  rai::KinematicWorld K;
+  K.addFile("../../rai-robotModels/baxter/baxter.g");
+  K.addObject("object", rai::ST_capsule, {.2, .05}, {1., 1., 0.}, -1., 0, {.8, .0, 1.});
+  arr q_home = K.getJointState();
+  FeatureSymbol fs_position = FS_position;
+  arr q,W,y,J;
+  q = q_home;
+  uint n = K.getJointStateDimension();
+  double w = rai::getParameter("w",1e-4);
+  W.setDiag(w,n);
+
+  arr q_zero = 0.*q_home;
+
+  RobotOperation B(K);
+  cout <<"joint names: " <<B.getJointNames() <<endl;
+  //B.move({q_zero}, {10.});
+  B.wait();
+  K.setJointState(q_zero);
+  rai::wait();
+
+  arr y_final = {0.8,.0,1.};
+  double steps = 10.;
+  arr y_target;
+  K.evalFeature(y, J, fs_position, {"left_lower_forearm"});
+  arr y_start = y;
+  
+  for(int i=1; i<steps; i++){
+    cout << "Eval Position " << i << endl;
+    K.evalFeature(y, J, fs_position, {"left_lower_forearm"});
+    
+    y_target = motionProfile(y_start,y_final,i,steps);
+    
+    //cout << "y: " << y << endl;
+
+    //cout << "y: " << y << " y_target: " << y_target << endl;
+    q += inverse(~J*J + W)*~J*((y_target - y)); 
+    //cout << "new joints: " << q << endl;
+    B.move({q},{10./steps});
+    B.wait();
+    K.setJointState(q);
+  }
+  
+
+  //q_home(-1) = .1; //last joint set to .1: left gripper opens 10cm (or 20cm?)
+  //B.move({q_home}, {4.});
+  //B.wait();
+
+
+  rai::wait();
+}
+
+
+void graspObject1(){
+  rai::KinematicWorld K;
+  K.addFile("../../rai-robotModels/baxter/baxter.g");
+  K.addObject("object", rai::ST_capsule, {.2, .05}, {1., 1., 0.}, -1., 0, {.8, .0, 1.});
+  //K.addObject("object", rai::ST_ssBox, {.1,.3,.2, .001}, {1., 1., 0.}, -1., 0, {.8, .0, 1.},{.7,.7,.0,.0});
+  arr q_home = K.getJointState();
+  
+  FeatureSymbol fs_position = FS_position;
+  FeatureSymbol fs_vectorZ = FS_vectorZ;
+  FeatureSymbol fs_qItself = FS_qItself;
+  arr q,W,y,J,Phi,PhiJ;
+  q = q_home;
+  uint n = K.getJointStateDimension();
+  double w = rai::getParameter("w",1e-4);
+  W.setDiag(w,n);
+
+  arr q_zero = 0.*q_home;
+
+  RobotOperation B(K);
+  cout << "joint names: " << B.getJointNames() << endl;
+  cout << "Frame names: " << K.getFrameNames() << endl;
+  cout << "Frame: " << K.getFrameByName("left_lower_forearm") << endl;
+  rai::wait();
+
+  
+
+  arr y_final = {0.8,.0,1.};
+  
+  double steps = 100.;
+  arr y_target, yvec,Jvec;
+
+  // Orientation of arm
+  arr vec_start, vec_target;
+  arr vec_final = {-1.,0.,0.};
+  K.evalFeature(vec_start, J, fs_vectorZ, {"left_lower_forearm"});
+
+  // Position of arm 
+  arr pos_start;
+  K.evalFeature(pos_start, J, fs_position, {"left_lower_forearm"});
+  pos_start += vec_start*-0.4;
+
+
+  arr qcurrent, Jcurrent;
+
+
+
+
+  
+  for(int i=1; i<steps; i++){
+    Phi.clear(); 
+    PhiJ.clear();
+
+    // Initial position of robot
+    K.evalFeature(qcurrent,Jcurrent, fs_qItself, {});
+    Phi.append((q_home - qcurrent)/100.);
+    PhiJ.append(Jcurrent/100.);
+
+
+
+    // Orientation of arm
+    //K.evalFeature(yvec, Jvec,, {"left_lower_forearm"});
+    K.evalFeature(yvec, Jvec, fs_vectorZ, {"left_lower_forearm"});
+    vec_target = motionProfile(vec_start,vec_final,i,steps);
+    Phi.append(vec_target - yvec);
+    PhiJ.append(Jvec);
+
+    
+    // Position of arm
+    K.evalFeature(y, J, fs_position, {"left_lower_forearm"});
+    //K.kinematicsPos(y, J, K.getFrameByName("left_lower_forearm"),{0.,0.,0.});
+    y_target = motionProfile(pos_start,y_final,i,steps);
+    Phi.append(y_target - (y+yvec*-0.4));
+    PhiJ.append(J);
+
+
+    // Calculate Minimum...
+    q += inverse(~PhiJ*PhiJ + W)*~PhiJ* Phi;
+    K.setJointState(q);
+    //B.move({q},{10./steps});
+    //B.wait();
+    
+  }
+  
+  B.move({q},{10.});
+  B.wait();
+  //q_home(-1) = .1; //last joint set to .1: left gripper opens 10cm (or 20cm?)
+  //B.move({q_home}, {4.});
+  //B.wait();
+
+
+  rai::wait();
+}
+
+
+
+
+
+void graspObject2(){
+  rai::KinematicWorld K;
+  K.addFile("../../rai-robotModels/baxter/baxter.g");
+  K.addObject("object", rai::ST_capsule, {.2, .05}, {1., 1., 0.}, -1., 0, {.8, .0, 1.}/*,{0.7,.7,0,0}*/);
+  arr q_home = K.getJointState();
+  
+  arr q,W,y,J,Phi,PhiJ;
+  uint n = K.getJointStateDimension();
+  double w = rai::getParameter("w",1e-4);
+  W.setDiag(w,n);
+
+  RobotOperation B(K);
+  //q = B.getJointPositions();
+  q = q_home;
+  //q = q*0.;
+  //B.move({q},{4.});
+  //B.wait();
+
+  K.setJointState(q);
+  rai::wait();
+
+  
+
+  arr y_final = {0.8,.0,1.};
+  
+  double steps = 100.;
+  arr y_target, yvec,Jvec;
+
+  // Orientation of arm
+  arr vec_start, vec_target;
+  arr vec_final = {-1.,0.,0.};
+
+  // Position of arm 
+  arr pos_start;
+  K.evalFeature(pos_start, J, FS_position, {"baxterL"});
+  //pos_start += vec_start*-0.4;
+
+
+  arr qcurrent, Jcurrent;
+
+  double gripper_start = q(-1);
+  double gripper_end = 0.1;
+
+
+  
+  for(int i=1; i<steps; i++){
+    Phi.clear(); 
+    PhiJ.clear();
+
+    // Initial position of robot
+    //K.evalFeature(qcurrent,Jcurrent, FS_qItself, {});
+    //Phi.append((q_home - qcurrent)/1000.);
+    //PhiJ.append(Jcurrent/1000.);
+
+
+
+    // Orientation of arm
+    K.evalFeature(yvec, Jvec, FS_scalarProductZZ, {"baxterL","object"});
+    Phi.append(yvec/100.);
+    PhiJ.append(Jvec/100.);
+
+
+    K.evalFeature(yvec, Jvec, FS_scalarProductXZ, {"baxterL","object"});
+    Phi.append(yvec/100.);
+    PhiJ.append(Jvec/100.);
+
+    
+    // Position of arm
+    K.evalFeature(y,J,FS_positionDiff,{"baxterL","object"});
+    Phi.append(y/30.);
+    PhiJ.append(J/30.);
+
+    //K.evalFeature(y, J, FS_position, {"baxterL"});
+    //y_target = motionProfile(pos_start,y_final,i,steps);
+    //Phi.append(y_target - (y));
+    //Phi.append(y);
+    //PhiJ.append(J);
+
+
+    // Calculate Minimum...
+    q -= 0.3*inverse(~PhiJ*PhiJ + W)*~PhiJ* Phi;
+    q(-1) = gripper_start + (i/steps)*(gripper_end - gripper_start);
+    K.setJointState(q);
+    //B.move({q},{10./steps});
+    //B.wait();
+    cout << "Iteration: " << i << endl;
+    
+  }
+
+  B.move({q},{6.});
+  B.wait();
+
+  // Closing Gripper
+  q(-1)=0.;
+  B.move({q},{3.});
+  B.wait();
+
+
+  // Move toq-zero
+  q = q_home;
+  q(-1)=0.;
+  B.move({q},{6.});
+  B.wait();
+
+
+  rai::wait();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main(int argc,char **argv){
   rai::initCmdLine(argc,argv);
 
-//  minimal_use();
+  //minimal_use();
 
   spline_use();
+
+  //graspObject();
+
+  //graspObject1();
+
+
+  //graspObject2();
 
   return 0;
 }
