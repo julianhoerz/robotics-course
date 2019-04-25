d88c331e7f76257dbb7936ef29304326fa0211b6
Julian <julianhoerz@gmail.com>
2019-04-25 21:11:01 +0200

    first opencv progress

--- cpp/p2-cameraMini/main.cpp
+++ cpp/p2-cameraMini/main.cpp
@@ -1,3 +1,12 @@
+#ifndef RAI_OPENCV
+  #define RAI_OPENCV 1
+#endif
+#ifndef RAI_ROS
+  #define RAI_ROS 1
+#endif
+
+
+
 #include <Perception/opencv.h> //always include this first!
 #include <Perception/opencvCamera.h>
 #include <Perception/depth2PointCloud.h>
@@ -10,7 +19,7 @@ void minimal_use(){
   Var<floatA> depth;
   
 
-#if 0 //using ros
+#if 1 //using ros
   RosCom ROS;
   SubscriberConv<sensor_msgs::Image, byteA, &conv_image2byteA> subRgb(rgb, "/camera/rgb/image_rect_color");
 //  SubscriberConv<sensor_msgs::Image, floatA, &conv_imageu162floatA> subDepth(depth, "/camera/depth_registered/image_raw");
@@ -22,7 +31,36 @@ void minimal_use(){
   for(uint i=0;i<100;i++){
     cv::Mat img = CV(rgb.get());
     if(img.total()>0){
-      cv::imshow("RGB", img);
+      cv::Mat hsv_image;
+      cv::cvtColor(img, hsv_image, cv::COLOR_BGR2HSV);
+      cv::Mat orig_image = img.clone();
+      cv::Mat lower_red_hue_range;
+      cv::Mat upper_red_hue_range;
+      cv::inRange(hsv_image, cv::Scalar(0, 100, 100), cv::Scalar(10, 255, 255), lower_red_hue_range);
+      cv::inRange(hsv_image, cv::Scalar(160, 100, 100), cv::Scalar(179, 255, 255), upper_red_hue_range);
+
+      cv::Mat red_hue_image;
+      cv::addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
+      cv::GaussianBlur(red_hue_image, red_hue_image, cv::Size(9, 9), 2, 2);
+
+      std::vector<cv::Vec3f> circles;
+      cv::HoughCircles(red_hue_image, circles, CV_HOUGH_GRADIENT, 1, red_hue_image.rows/8, 100, 20, 0, 0);
+
+
+      if(circles.size() == 0){
+        cout << "Endend" << endl;
+        continue;
+      } 
+      for(size_t current_circle = 0; current_circle < circles.size(); ++current_circle) {
+        cv::Point center(std::round(circles[current_circle][0]), std::round(circles[current_circle][1]));
+        int radius = std::round(circles[current_circle][2]);
+        cv::circle(orig_image, center, radius, cv::Scalar(0, 255, 0), 5);
+      }
+
+
+      cv::imshow("RGB", orig_image);
+
+
       cv::waitKey(1);
     }
     rai::wait(.1);
