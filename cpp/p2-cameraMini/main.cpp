5547d0ae9155157e10283ac89f03ae5b822b74ba
Julian <julianhoerz@gmail.com>
2019-04-30 11:14:51 +0200

    devel

--- cpp/p2-cameraMini/main.cpp
+++ cpp/p2-cameraMini/main.cpp
@@ -17,18 +17,20 @@ void minimal_use(){
 
   Var<byteA> rgb;
   Var<floatA> depth;
+
+  int cnt = 0;
   
 
-#if 1 //using ros
-  RosCom ROS;
-  SubscriberConv<sensor_msgs::Image, byteA, &conv_image2byteA> subRgb(rgb, "/camera/rgb/image_rect_color");
-//  SubscriberConv<sensor_msgs::Image, floatA, &conv_imageu162floatA> subDepth(depth, "/camera/depth_registered/image_raw");
-#else //using a webcam
-  OpencvCamera cam(rgb);
-#endif
+  #if 0 //using ros
+    RosCom ROS;
+    SubscriberConv<sensor_msgs::Image, byteA, &conv_image2byteA> subRgb(rgb, "/camera/rgb/image_rect_color");
+  //  SubscriberConv<sensor_msgs::Image, floatA, &conv_imageu162floatA> subDepth(depth, "/camera/depth_registered/image_raw");
+  #else //using a webcam
+    OpencvCamera cam(rgb);
+  #endif
 
   //looping images through opencv
-  for(uint i=0;i<100;i++){
+  for(int i=0;i>-1;i++){
     cv::Mat img = CV(rgb.get());
     if(img.total()>0){
       cv::Mat hsv_image;
@@ -41,24 +43,32 @@ void minimal_use(){
 
       cv::Mat red_hue_image;
       cv::addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
-      cv::GaussianBlur(red_hue_image, red_hue_image, cv::Size(9, 9), 2, 2);
+      cv::GaussianBlur(red_hue_image, red_hue_image, cv::Size(9, 9), 30, 30);
 
       std::vector<cv::Vec3f> circles;
       cv::HoughCircles(red_hue_image, circles, CV_HOUGH_GRADIENT, 1, red_hue_image.rows/8, 100, 20, 0, 0);
 
-
+      //cv::ellipse( im2, Point( 120, 130 ), Size( 50.0, 60.0 ), 0, 0, 360, Scalar( 255, 255, 255), -1, 8 );
+      cv::Mat buff_image = orig_image;
+      buff_image.setTo(cv::Scalar(0,0,0));
       if(circles.size() == 0){
-        cout << "Endend" << endl;
-        continue;
+        cnt ++;
+        cout << "Skipping... " << cnt << endl;
       } 
-      for(size_t current_circle = 0; current_circle < circles.size(); ++current_circle) {
-        cv::Point center(std::round(circles[current_circle][0]), std::round(circles[current_circle][1]));
-        int radius = std::round(circles[current_circle][2]);
-        cv::circle(orig_image, center, radius, cv::Scalar(0, 255, 0), 5);
+      else{
+        for(size_t current_circle = 0; current_circle < circles.size(); ++current_circle) {
+          cv::Point center(std::round(circles[current_circle][0]), std::round(circles[current_circle][1]));
+          int radius = std::round(circles[current_circle][2]);
+          cv::circle(buff_image, center, radius, cv::Scalar(255, 255, 255), -20);
+        }
       }
+      cv::Mat res;
+      cv::bitwise_and(buff_image,red_hue_image,res);
 
 
+      cv::imshow("red_hue_image", red_hue_image);
       cv::imshow("RGB", orig_image);
+      cv::imshow("AND", buff_image);
 
 
       cv::waitKey(1);
