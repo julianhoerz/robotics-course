2df0b74ba36d3b121451d2c0626e55670b3b45df
Julian <julianhoerz@gmail.com>
2019-04-30 18:02:13 +0200

    detect red ball

--- cpp/p2-cameraMini/main.cpp
+++ cpp/p2-cameraMini/main.cpp
@@ -33,6 +33,7 @@ void minimal_use(){
   for(int i=0;i>-1;i++){
     cv::Mat img = CV(rgb.get());
     if(img.total()>0){
+      cv::medianBlur(img, img, 3);
       cv::Mat hsv_image;
       cv::cvtColor(img, hsv_image, cv::COLOR_BGR2HSV);
       cv::Mat orig_image = img.clone();
@@ -43,33 +44,67 @@ void minimal_use(){
 
       cv::Mat red_hue_image;
       cv::addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
-      cv::GaussianBlur(red_hue_image, red_hue_image, cv::Size(9, 9), 30, 30);
+      cv::GaussianBlur(red_hue_image, red_hue_image, cv::Size(9, 9), 100, 100);
 
       std::vector<cv::Vec3f> circles;
       cv::HoughCircles(red_hue_image, circles, CV_HOUGH_GRADIENT, 1, red_hue_image.rows/8, 100, 20, 0, 0);
 
       //cv::ellipse( im2, Point( 120, 130 ), Size( 50.0, 60.0 ), 0, 0, 360, Scalar( 255, 255, 255), -1, 8 );
-      cv::Mat buff_image = orig_image;
-      buff_image.setTo(cv::Scalar(0,0,0));
+      cv::Mat buff_image = orig_image.clone();
+      cv::Mat buff2_image = red_hue_image.clone();
+      cv::Mat mymask;
+
+      //buff_image.setTo(cv::Scalar(0,0,0));
       if(circles.size() == 0){
         cnt ++;
         cout << "Skipping... " << cnt << endl;
       } 
       else{
+
+        //for(size_t current_circle = 0; current_circle < circles.size(); ++current_circle) {
         for(size_t current_circle = 0; current_circle < circles.size(); ++current_circle) {
           cv::Point center(std::round(circles[current_circle][0]), std::round(circles[current_circle][1]));
           int radius = std::round(circles[current_circle][2]);
-          cv::circle(buff_image, center, radius, cv::Scalar(255, 255, 255), -20);
+          if((center.x - radius) < 0 || 
+            (center.y - radius) < 0 ||  
+            (center.x + radius) > img.cols || 
+            (center.y + radius) > img.rows){
+              //cout << "Skipping... big circle" << endl;
+              continue;
+          }
+          else{
+            cv::Mat mask(cv::Size(radius*2,radius*2), CV_8U, cv::Scalar(0)); // all black
+            cv::circle(mask, cv::Point(radius,radius), radius, cv::Scalar(255), -1);
+            cv::Rect region(center.x-radius,center.y-radius,radius*2,radius*2); // example roi
+            cv::Mat roi(red_hue_image, region); 
+            cv::bitwise_and(roi, roi, buff2_image, mask);
+            cout << "Typical mean: " << cv::mean(mask)[0] - cv::mean(buff2_image)[0] << endl;
+            if(cv::mean(cv::mean(mask)[0] - buff2_image)[0] < 100){
+              //cout << "real circle" << endl;
+              cv::circle(buff_image, center, radius, cv::Scalar(255, 255, 255), 5);
+            }
+            else{
+              // 
+              //cv::circle(buff_image, center, radius, cv::Scalar(0, 255, 0), 5);
+            }
+
+            
+            mymask =mask;
+          }
+
+
         }
       }
       cv::Mat res;
-      cout << buff_image.shape
-      cv::bitwise_and(buff_image,red_hue_image,res);
+      //cout << buff_image.shape
+      //cv::bitwise_and(,red_hue_image,res,buff_image);
 
 
       cv::imshow("red_hue_image", red_hue_image);
       cv::imshow("RGB", orig_image);
       cv::imshow("AND", buff_image);
+      //cv::imshow("Mask: ", mymask);
+      //cv::imshow("Mask2: ", buff2_image);
 
 
       cv::waitKey(1);
